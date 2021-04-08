49d5006295d22c93fa5bb82fde1437112a3d8f45
[tekilit] <67770867+tekilit@users.noreply.github.com>
2021-04-08 21:21:59 +0200

    new model favorites

--- src/models/favorites.js
+++ src/models/favorites.js
@@ -2,20 +2,21 @@ const monggose = require('mongoose');
 const { Schema } = mongoose;
 
 const FavoritesSchema = new Schema ({
-    UserId: {
-        type: monggose.Types.ObjectId,
+    userId: {
+        type: mongoose.Types.ObjectId,
         ref: 'Users',
         unique: true,
         required: true
     },
-    Products: {
-        type: monggose.Types.ObjectId,
+    products: [{
+        type: mongoose.Types.ObjectId,
         ref: 'Products',
-        unique: true,
 
-    },
-    timestamp: true
-});
+    }],
+},
+    {
+    timestamps: true
+    });
 
 const Favorites = mongoose.model('Favorites', FavoritesSchema);
 module.exports = Favorites;
