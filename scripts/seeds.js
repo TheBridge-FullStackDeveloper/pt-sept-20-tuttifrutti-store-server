9c8b4f7d5403f2ba9f00642b93ec837c6e5ec3af
Fernando Carmona <fernando.carayu@gmail.com>
2021-04-09 18:05:27 +0200

    add users seed and prettify db config

--- scripts/seeds.js
+++ scripts/seeds.js
@@ -1,12 +1,14 @@
-require("../src/configs/db");
+require('../src/configs/db');
 
-const { createUsers } = require("./users");
+const { createUsers } = require('./users');
 
 const DEFAULT_ROWS = 5;
 const SEED = 123;
 
-try {
-  createUsers(DEFAULT_ROWS, SEED);
-} catch (error) {
-  console.error(error);
-}
+(async () => {
+  try {
+    await createUsers(DEFAULT_ROWS, SEED);
+  } catch (error) {
+    console.error(error);
+  }
+})();
