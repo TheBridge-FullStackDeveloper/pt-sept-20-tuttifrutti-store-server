4aed6a4fd47e4d9c08792f31f9c16a26d828f152
Fernando Carmona <fernando.carayu@gmail.com>
2021-04-10 10:43:04 +0200

    endpoint to get product by id: GET /products/:id

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,9 +2,24 @@ const router = require('express').Router();
 
 const ProductModel = require('../../models/Products');
 
-router.get('/', async (req, res, next) => {
+router.get('/', async (_, res, next) => {
   try {
     const result = await ProductModel.find({});
+
+    res.status(200).json({
+      success: true,
+      data: result
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const result = await ProductModel.findById(id);
 
     res.status(200).json({
       success: true,
