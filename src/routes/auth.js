881b3e66906afe2f1d78d315db396f71b2774f98
[tekilit] <67770867+tekilit@users.noreply.github.com>
2021-04-10 12:32:39 +0200

    login-logout

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -15,4 +15,23 @@ router.post('/register', (req, res, next) => {
   })(req, res, next);
 });
 
+router.post('/login', (req, res, next) => {
+  passport.authenticate('login', (err, user) => {
+    if (err) {
+      return res.status(402).json({ data: err.message, success: false });
+    }
+    req.logIn(user, (loginErr) => {
+      if (loginErr) {
+        return res.status(401).json({ data: loginErr.message, success: false });
+      }
+      res.status(200).json({ data: req.user, success: true });
+    });
+  })(req, res, next);
+});
+
+router.get('/logout', (req, res) => {
+  req.logout();
+
+  res.status(200).json({ data: 'OK' });
+});
 module.exports = router;
