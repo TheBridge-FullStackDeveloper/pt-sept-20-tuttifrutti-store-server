61d2f221ec6d4c393e213de2a017e5c75f1dcea7
Fernando Carmona <fernando.carayu@gmail.com>
2021-04-13 21:12:07 +0200

    add return

--- src/routes/favorites.js
+++ src/routes/favorites.js
@@ -14,7 +14,7 @@ router.get('/', [isAuthenticated], async (req, res, next) => {
         products: []
       });
 
-      res.status(200).json({
+      return res.status(200).json({
         success: true,
         count: result.products.length,
         data: { products: result.products }
