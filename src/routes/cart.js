1a63df48d16d58be8f7fcd6e5fdc3a4ef277bdf4
elisasoto <elisasotoperez@gmail.com>
2021-04-19 20:32:13 +0200

    added filtered products function || pending to resolve the "deleteOne" if filteredProducts is true

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -6,7 +6,7 @@ const { isAuthenticated } = require('../middlewares/authentication');
 
 router.put('/add/:productId', [isAuthenticated], async (req, res, next) => {
   const { productId } = req.params;
-  const { quantity } = req.query;
+  const quantity = req.query.quantity || 1;
 
   try {
     const numQuantity = Number(quantity);
@@ -53,6 +53,79 @@ router.put('/add/:productId', [isAuthenticated], async (req, res, next) => {
 
     if (isProductFound) {
       console.log('found', newProducts);
+      const result = await CartModel.findOneAndUpdate(
+        { userId: req.user },
+        { productsQuantity: newProducts },
+        { new: true }
+      );
+
+      return res.status(200).json({
+        success: true,
+        count: newProducts.length,
+        data: result
+      });
+    }
+
+    const result = await CartModel.findOneAndUpdate(
+      { userId: req.user },
+      {
+        $push: { productsQuantity: { productId, quantity: numQuantity } }
+      },
+      { new: true }
+    );
+
+    const products = result.get('productsQuantity');
+
+    res.status(200).json({
+      success: true,
+      count: products.length,
+      data: { products }
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put('/remove/:productId', [isAuthenticated], async (req, res, next) => {
+  const { productId } = req.params;
+  const quantity = req.query.quantity || 1;
+
+  try {
+    const numQuantity = Number(quantity);
+
+    if (Number.isNaN(numQuantity)) {
+      const error = new Error('not a number');
+      error.code = 400;
+      throw error;
+    }
+
+    const userCart = await CartModel.findOne({ userId: req.user });
+
+    // Transformar el array del subesquema en un array de objetos reales de JS
+    const prevProductsQuantity = userCart
+      .get('productsQuantity')
+      .map((el) => el.toObject());
+
+    let isProductFound = false;
+
+    const newProducts = prevProductsQuantity.map((product) => {
+      if (product.productId.toString() === productId) {
+        isProductFound = true;
+        return { ...product, quantity: product.quantity - numQuantity };
+      }
+      return product;
+    });
+    const filteredProducts = prevProductsQuantity.filter((product) => {
+      if (product.productId.toString() === productId && product.quantity <= 1) {
+        isProductFound = true;
+        return product.productId.toString() !== productId;
+      }
+      return product;
+    });
+
+    console.log('filtered products', filteredProducts);
+
+    if (isProductFound) {
       const result = await CartModel.findOneAndUpdate(
         { userId: req.user },
         { productsQuantity: newProducts },
